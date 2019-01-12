aa31e193a8054f4604481facc8a2f81470b4f774
Kurtchekov <kurtchekov@gmail.com>
2019-01-13 00:17:55 +0200

    Initial Release

---
+++ Turbine Calculator/TurbineCalculator.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Turbine_Calculator {
+    partial class TurbineCalculator {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sicCheck = new System.Windows.Forms.CheckBox();
+            this.extremeCheck = new System.Windows.Forms.CheckBox();
+            this.steelCheck = new System.Windows.Forms.CheckBox();
+            this.statorCheck = new System.Windows.Forms.CheckBox();
+            this.lengthInput = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.fuelList = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.fuelMode = new System.Windows.Forms.TabControl();
+            this.autoMode = new System.Windows.Forms.TabPage();
+            this.manualMode = new System.Windows.Forms.TabPage();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.rfpermbInput = new System.Windows.Forms.NumericUpDown();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.expansionInput = new System.Windows.Forms.NumericUpDown();
+            this.runBTN = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lengthInput)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.fuelMode.SuspendLayout();
+            this.autoMode.SuspendLayout();
+            this.manualMode.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rfpermbInput)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.expansionInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sicCheck);
+            this.groupBox1.Controls.Add(this.extremeCheck);
+            this.groupBox1.Controls.Add(this.steelCheck);
+            this.groupBox1.Controls.Add(this.statorCheck);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Blade Types to be used";
+            // 
+            // sicCheck
+            // 
+            this.sicCheck.AutoSize = true;
+            this.sicCheck.Checked = true;
+            this.sicCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.sicCheck.Location = new System.Drawing.Point(7, 89);
+            this.sicCheck.Name = "sicCheck";
+            this.sicCheck.Size = new System.Drawing.Size(71, 17);
+            this.sicCheck.TabIndex = 3;
+            this.sicCheck.Text = "SiC Rotor";
+            this.sicCheck.UseVisualStyleBackColor = true;
+            // 
+            // extremeCheck
+            // 
+            this.extremeCheck.AutoSize = true;
+            this.extremeCheck.Checked = true;
+            this.extremeCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.extremeCheck.Location = new System.Drawing.Point(7, 66);
+            this.extremeCheck.Name = "extremeCheck";
+            this.extremeCheck.Size = new System.Drawing.Size(93, 17);
+            this.extremeCheck.TabIndex = 2;
+            this.extremeCheck.Text = "Extreme Rotor";
+            this.extremeCheck.UseVisualStyleBackColor = true;
+            // 
+            // steelCheck
+            // 
+            this.steelCheck.AutoSize = true;
+            this.steelCheck.Checked = true;
+            this.steelCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.steelCheck.Location = new System.Drawing.Point(7, 43);
+            this.steelCheck.Name = "steelCheck";
+            this.steelCheck.Size = new System.Drawing.Size(79, 17);
+            this.steelCheck.TabIndex = 1;
+            this.steelCheck.Text = "Steel Rotor";
+            this.steelCheck.UseVisualStyleBackColor = true;
+            // 
+            // statorCheck
+            // 
+            this.statorCheck.AutoSize = true;
+            this.statorCheck.Checked = true;
+            this.statorCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.statorCheck.Location = new System.Drawing.Point(7, 20);
+            this.statorCheck.Name = "statorCheck";
+            this.statorCheck.Size = new System.Drawing.Size(54, 17);
+            this.statorCheck.TabIndex = 0;
+            this.statorCheck.Text = "Stator";
+            this.statorCheck.UseVisualStyleBackColor = true;
+            // 
+            // lengthInput
+            // 
+            this.lengthInput.Location = new System.Drawing.Point(6, 19);
+            this.lengthInput.Name = "lengthInput";
+            this.lengthInput.Size = new System.Drawing.Size(92, 20);
+            this.lengthInput.TabIndex = 1;
+            this.lengthInput.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lengthInput);
+            this.groupBox2.Location = new System.Drawing.Point(362, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(104, 48);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Shaft Length";
+            // 
+            // fuelList
+            // 
+            this.fuelList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.fuelList.FormattingEnabled = true;
+            this.fuelList.Items.AddRange(new object[] {
+            "High Pressure Steam",
+            "Low Pressure Steam",
+            "Steam"});
+            this.fuelList.Location = new System.Drawing.Point(3, 3);
+            this.fuelList.Name = "fuelList";
+            this.fuelList.Size = new System.Drawing.Size(177, 52);
+            this.fuelList.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.fuelMode);
+            this.groupBox3.Location = new System.Drawing.Point(153, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(203, 117);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Fuel";
+            // 
+            // fuelMode
+            // 
+            this.fuelMode.Controls.Add(this.autoMode);
+            this.fuelMode.Controls.Add(this.manualMode);
+            this.fuelMode.Location = new System.Drawing.Point(7, 19);
+            this.fuelMode.Name = "fuelMode";
+            this.fuelMode.SelectedIndex = 0;
+            this.fuelMode.Size = new System.Drawing.Size(191, 88);
+            this.fuelMode.TabIndex = 0;
+            // 
+            // autoMode
+            // 
+            this.autoMode.Controls.Add(this.fuelList);
+            this.autoMode.Location = new System.Drawing.Point(4, 22);
+            this.autoMode.Name = "autoMode";
+            this.autoMode.Padding = new System.Windows.Forms.Padding(3);
+            this.autoMode.Size = new System.Drawing.Size(183, 62);
+            this.autoMode.TabIndex = 0;
+            this.autoMode.Text = "Auto Mode";
+            this.autoMode.UseVisualStyleBackColor = true;
+            // 
+            // manualMode
+            // 
+            this.manualMode.Controls.Add(this.groupBox5);
+            this.manualMode.Controls.Add(this.groupBox4);
+            this.manualMode.Location = new System.Drawing.Point(4, 22);
+            this.manualMode.Name = "manualMode";
+            this.manualMode.Padding = new System.Windows.Forms.Padding(3);
+            this.manualMode.Size = new System.Drawing.Size(183, 62);
+            this.manualMode.TabIndex = 1;
+            this.manualMode.Text = "Manual Mode";
+            this.manualMode.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.rfpermbInput);
+            this.groupBox5.Location = new System.Drawing.Point(93, 6);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(81, 48);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "RF/mb";
+            // 
+            // rfpermbInput
+            // 
+            this.rfpermbInput.Location = new System.Drawing.Point(6, 19);
+            this.rfpermbInput.Name = "rfpermbInput";
+            this.rfpermbInput.Size = new System.Drawing.Size(69, 20);
+            this.rfpermbInput.TabIndex = 1;
+            this.rfpermbInput.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.expansionInput);
+            this.groupBox4.Location = new System.Drawing.Point(6, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(81, 48);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Expansion";
+            // 
+            // expansionInput
+            // 
+            this.expansionInput.Location = new System.Drawing.Point(6, 19);
+            this.expansionInput.Name = "expansionInput";
+            this.expansionInput.Size = new System.Drawing.Size(69, 20);
+            this.expansionInput.TabIndex = 1;
+            this.expansionInput.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // runBTN
+            // 
+            this.runBTN.Location = new System.Drawing.Point(362, 66);
+            this.runBTN.Name = "runBTN";
+            this.runBTN.Size = new System.Drawing.Size(104, 63);
+            this.runBTN.TabIndex = 5;
+            this.runBTN.Text = "Run!";
+            this.runBTN.UseVisualStyleBackColor = true;
+            this.runBTN.Click += new System.EventHandler(this.runBTN_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(13, 136);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(453, 223);
+            this.output.TabIndex = 6;
+            // 
+            // TurbineCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 374);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.runBTN);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TurbineCalculator";
+            this.Text = "Turbine Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lengthInput)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.fuelMode.ResumeLayout(false);
+            this.autoMode.ResumeLayout(false);
+            this.manualMode.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rfpermbInput)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.expansionInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox sicCheck;
+        private System.Windows.Forms.CheckBox extremeCheck;
+        private System.Windows.Forms.CheckBox steelCheck;
+        private System.Windows.Forms.CheckBox statorCheck;
+        private System.Windows.Forms.NumericUpDown lengthInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox fuelList;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TabControl fuelMode;
+        private System.Windows.Forms.TabPage autoMode;
+        private System.Windows.Forms.TabPage manualMode;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.NumericUpDown rfpermbInput;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown expansionInput;
+        private System.Windows.Forms.Button runBTN;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ Turbine Calculator/TurbineCalculator.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Turbine_Calculator {
+    public partial class TurbineCalculator : Form {
+        public TurbineCalculator() {
+            InitializeComponent();
+            fuelList.SelectedIndex = 0;
+        }
+
+        public static double fuelExpansion = 4;
+        public static double rfpermb = 16;
+        public static int segments = 20;
+        public static double[] targets;
+
+        public static Blade stator = new Blade(0, "stator", .75, -1, true);
+        public static Blade steel = new Blade(1, "steel", 1.4, 1, false);
+        public static Blade extreme = new Blade(2, "extreme", 1.6, 1.1, false);
+        public static Blade SiC = new Blade(3, "SiC", 1.8, 1.25, false);
+
+        public static List<Blade> blades = new List<Blade>();
+
+        public static Blade[] currentBlades;
+        public static double[] currentCoefficients;
+        public static double[] currentEfficiencySum;
+        public static int[] rotors;
+        public static Blade[] bestBlades;
+        public static double bestEfficiency;
+        public static double bestExpansion;
+        public static int bladesAvailable;
+
+        private void runBTN_Click(object sender, EventArgs e) {
+            Setup();
+            targets = new double[segments];
+            //output.Text += "target values for each segment:\r\n";
+            for (int segment = 0; segment < segments; segment++) {
+                targets[segment] = Math.Pow(fuelExpansion, ((segment + .5) / segments));
+                //output.Text += segment + ": " + targets[segment] + "\r\n";
+            }
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Calculate();
+            output.Text += "Best combination for fuel expansion " + fuelExpansion + " and " + segments + " blocks long shaft:\r\n";
+            string list = "";
+            for (int x = 0; x < segments; x++) list += bestBlades[x].name + ", ";
+            list = list.Substring(0, list.Length - 2);
+            output.Text += list + "\r\n";
+            output.Text += (fuelExpansion * bestExpansion).ToString("P") +
+                " [" + fuelExpansion + " x " + bestExpansion.ToString("P") + "]\r\n";
+            output.Text += "RF per mb (coil efficiency not taken into account!): " + rfpermb * bestEfficiency + "\r\n";
+            output.Text += "Time taken: " + stopwatch.ElapsedMilliseconds + " milliseconds\r\n";
+        }
+
+        public void Setup() {
+            bestExpansion = 0;
+            bestEfficiency = 0;
+            blades.Clear();
+            if (statorCheck.Checked) blades.Add(stator);
+            if (steelCheck.Checked) blades.Add(steel);
+            if (extremeCheck.Checked) blades.Add(extreme);
+            if (sicCheck.Checked) blades.Add(SiC);
+            segments = (int)lengthInput.Value;
+            if (fuelMode.SelectedTab == autoMode) {
+                switch (fuelList.SelectedIndex) {
+                    case 0: //High Pressure Steam
+                    fuelExpansion = 4;
+                    rfpermb = 16;
+                    break;
+                    case 1: //Low Pressure Steam
+                    fuelExpansion = 2;
+                    rfpermb = 4;
+                    break;
+                    case 2: //Low Pressure Steam
+                    fuelExpansion = 2;
+                    rfpermb = 4;
+                    break;
+                }
+            } else {
+                fuelExpansion = (int)expansionInput.Value;
+                rfpermb = (int)rfpermbInput.Value;
+            }
+            output.Text = "";
+        }
+
+        public static void Calculate() {
+            currentBlades = new Blade[segments];
+            bestBlades = new Blade[segments];
+            for (int blade = 0; blade < segments; blade++) bestBlades[blade] = currentBlades[blade] = blades[0];
+            currentCoefficients = new double[segments];
+            currentEfficiencySum = new double[segments];
+            rotors = new int[segments];
+            bladesAvailable = blades.Count;
+            recursiveCheck(0);
+        }
+
+        public static bool checkLowerBound(int depth, Blade currentStator) {
+            double bestCurrentEfficiency = 0;
+            double bestLaterEfficiency = 0;
+            for (int blade = 0; blade < bladesAvailable; blade++) {
+                Blade actualBlade = blades[blade];
+                if (actualBlade.isStator) continue;
+                double currentCoefficient = currentCoefficients[depth - 1] * actualBlade.coefficient;
+                double currentEfficiency = (Math.Min(currentCoefficient, targets[depth]) /
+                        Math.Max(currentCoefficient, targets[depth])) * actualBlade.efficiency;
+                if (bestCurrentEfficiency < currentEfficiency) bestCurrentEfficiency = currentEfficiency;
+                double laterCoefficient = currentCoefficients[depth - 1] * currentStator.coefficient * actualBlade.coefficient;
+                double laterEfficiency = (Math.Min(laterCoefficient, targets[depth + 1]) /
+                        Math.Max(laterCoefficient, targets[depth + 1])) * actualBlade.efficiency;
+                if (bestLaterEfficiency < laterEfficiency) bestLaterEfficiency = laterEfficiency;
+            }
+            return bestLaterEfficiency > bestCurrentEfficiency;
+        }
+
+        public static void recursiveCheck(int depth) {
+            for (int blade = 0; blade < bladesAvailable; blade++) {
+                Blade actualBlade = blades[blade];
+                currentBlades[depth] = actualBlade;
+
+                if (depth == 0) {
+                    currentCoefficients[0] = actualBlade.coefficient;
+                    if (!actualBlade.isStator) {
+                        currentEfficiencySum[0] = (Math.Min(currentCoefficients[0], targets[0]) /
+                            Math.Max(currentCoefficients[0], targets[0])) * actualBlade.efficiency;
+                        rotors[0] = 1;
+                    } else {
+                        rotors[0] = 0;
+                        currentEfficiencySum[0] = 0;
+                    }
+                } else {
+                    currentCoefficients[depth] = currentCoefficients[depth - 1] * actualBlade.coefficient;
+                    if (!actualBlade.isStator) {
+                        if (targets[depth] < currentCoefficients[depth - 1]) continue;
+                        currentEfficiencySum[depth] = currentEfficiencySum[depth - 1] + (
+                        (Math.Min(currentCoefficients[depth], targets[depth]) /
+                        Math.Max(currentCoefficients[depth], targets[depth])) * actualBlade.efficiency);
+                        rotors[depth] = rotors[depth - 1] + 1;
+                    } else {
+                        if (targets[depth] > currentCoefficients[depth - 1] &&
+                            depth + 1 < segments) if (!checkLowerBound(depth, actualBlade)) continue;
+                        rotors[depth] = rotors[depth - 1];
+                        currentEfficiencySum[depth] = currentEfficiencySum[depth - 1];
+                    }
+                }
+
+                if (depth + 1 < segments) recursiveCheck(depth + 1);
+                else { //last segment
+                    double averageEfficiency = currentEfficiencySum[depth] / rotors[depth];
+                    double expansionCoefficient = Math.Min(currentCoefficients[depth], fuelExpansion) / Math.Max(currentCoefficients[depth], fuelExpansion);
+                    double finalEfficiency = averageEfficiency * expansionCoefficient;
+                    if (finalEfficiency > bestEfficiency) {
+                        bestEfficiency = finalEfficiency;
+                        bestExpansion = expansionCoefficient;
+                        for (int x = 0; x < segments; x++) bestBlades[x] = currentBlades[x];
+                    }
+                }
+            }
+        }
+    }
+
+    public struct Blade {
+
+        public int uid;
+        public string name;
+        public double coefficient;
+        public double efficiency;
+        public bool isStator;
+
+        /// <param name="u">A Unique IDentifier</param>
+        /// <param name="n">Human-friendly name</param>
+        /// <param name="c">Coefficient number</param>
+        /// <param name="f">Efficiency Number</param>
+        /// <param name="s">Is this an stator?</param>
+        public Blade(int u, string n, double c, double f, bool s) {
+            uid = u;
+            name = n;
+            coefficient = c;
+            efficiency = f;
+            isStator = s;
+        }
+
+        public static bool operator ==(Blade obj1, Blade obj2) {
+            return (obj1.uid == obj2.uid);
+        }
+
+        public static bool operator !=(Blade obj1, Blade obj2) {
+            return (obj1.uid != obj2.uid);
+        }
+
+        public override bool Equals(Object obj) {
+            return (obj is Blade) && ((Blade)obj).uid == uid;
+        }
+
+        public override int GetHashCode() {
+            return uid;
+        }
+    }
+}
